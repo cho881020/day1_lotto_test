21f1a28d2327e3d21ced6d74eaba997e8e9885ee
조경진 <cho881020@gmail.com>
2022-10-19 14:10:49 +0900

    범위 검사 탈락시 안내 추가

--- lotto.py
+++ lotto.py
@@ -18,8 +18,14 @@ for i in range(6):
         # 받아낸 input_num가 제대로 되었다면? 무한반복 종료 => 다음 숫자 받으러.
 
         # 반복종료 조건 1. 1~45 이내?
+        # 1 <= 입력값 and 입력값 <= 45
 
-        is_range_ok = True  # 임시로 괜찮다고 설정
+        is_range_ok = (1 <= input_num) and (input_num <= 45)
 
         if is_range_ok:
+            # 검사 통과시 무한반복 종료
             break
+        else:
+            # 범위 검사 탈락시 안내문구
+            print('1~45의 값만 입력 가능합니다.')
+            # 다시 입력 시킨다 => 무한반복 유지 => break X
